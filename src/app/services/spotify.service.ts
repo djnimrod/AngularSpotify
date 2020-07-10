6d217ed9587128ce37111f6fdcf47ce0d36d19dd
danielDev <valcyum.085@gmail.com>
2020-07-10 17:48:29 -0400

    home m

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -20,4 +20,18 @@ export class SpotifyService {
       }
     );
   }
+
+  getArtista(termino: string) {
+    const headers = new HttpHeaders({
+      Authorization:
+        "Bearer BQA_96-DaiWL4q9rNxh-h_hx5CRM5YJaRKifTK6itprRA8Zgst1QnQV3mWlYSTXO6y4LZVj3cw3EClqRtck",
+    });
+
+    return this.http.get(
+      "https://api.spotify.com/v1/browse/new-releases?limit=20",
+      {
+        headers,
+      }
+    );
+  }
 }
